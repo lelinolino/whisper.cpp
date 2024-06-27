4b2ee356d83d2c3d30683279c584b66bbea8a269
lelinolino <lelinolino@users.noreply.github.com>
2024-06-27 18:44:45 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -49,8 +49,8 @@ let package = Package(
                 // NOTE: NEW_LAPACK will required iOS version 16.4+
                 // We should consider add this in the future when we drop support for iOS 14
                 // (ref: ref: https://developer.apple.com/documentation/accelerate/1513264-cblas_sgemm?language=objc)
-                .define("ACCELERATE_NEW_LAPACK"),
-                .define("ACCELERATE_LAPACK_ILP64")
+                //.define("ACCELERATE_NEW_LAPACK"),
+                //.define("ACCELERATE_LAPACK_ILP64")
             ],
             linkerSettings: [
                 .linkedFramework("Accelerate")
